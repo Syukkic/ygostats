8317ed2fed4f2e79d28d5cea599b0efbaeda58de
Yuki.N <Syukkic@gmail.com>
2025-11-25 15:50:03 +0800

    chore: add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
 	let { children } = $props();
 </script>
 
 <svelte:head>
+	<link rel="icon" href={favicon} />
 	<title>YGOSTATS</title>
 </svelte:head>
 
